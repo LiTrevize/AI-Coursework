11f4824fa60acc175b4b782ee1001531bb32e50d
LiTrevize <universeofjimmy@gmail.com>
2019-11-01 18:53:50 +0800

    add living cost

--- minimax/chessboard.py
+++ minimax/chessboard.py
@@ -3,13 +3,14 @@ import math
 global maxDepth
 global N
 INF = 100
+moves = []
 
 
-def childStates(r1,c1,r2,c2,turn1):
+def childStates(r1, c1, r2, c2, turn1):
     validMove = []
     if turn1:
         if 1 <= r1+1 <= N and 1 <= c1 <= N:
-            validMove.append((r1+1,c1,r2,c2))
+            validMove.append((r1+1, c1, r2, c2))
         if 1 <= r1-1 <= N and 1 <= c1 <= N:
             validMove.append((r1-1, c1, r2, c2))
         if 1 <= r1 <= N and 1 <= c1+1 <= N:
@@ -36,32 +37,39 @@ def childStates(r1,c1,r2,c2,turn1):
     return validMove
 
 
+# reward: 100 for A win, -100 for B win, 0 for draw
 
-# reward: 10 for A win, -10 for B win, 0 for draw
 
-
-def minimax(r1, c1, r2, c2, turn1=True, curDepth=1,alpha=-INF,beta=INF):
+def minimax(r1, c1, r2, c2, turn1=True, curDepth=1, alpha=-INF, beta=INF):
     # game over
     if r1 == r2 and c1 == c2:
         if turn1:
-            return -10
+            return -100+curDepth
         else:
-            return 10
+            return 100-curDepth
     # maxDepth reached
     if curDepth == maxDepth:
-        return 0
+        if turn1:
+            return -curDepth
+        else:
+            return curDepth
     # A's turn, maximize
     if turn1:
-        for move in childStates(r1,c1,r2,c2,True):
-            value = minimax(move[0],move[1],move[2],move[3],False,curDepth+1,alpha,beta)
+        for move in childStates(r1, c1, r2, c2, True):
+            value = minimax(move[0], move[1], move[2],
+                            move[3], False, curDepth+1, alpha, beta)
+            # pruning
             if value > alpha:
                 alpha = value
             if alpha >= beta:
                 break
         return alpha
+    # B's turn, minimize
     else:
-        for move in childStates(r1,c1,r2,c2,False):
-            value = minimax(move[0],move[1],move[2],move[3],True,curDepth+1,alpha,beta)
+        for move in childStates(r1, c1, r2, c2, False):
+            value = minimax(move[0], move[1], move[2],
+                            move[3], True, curDepth+1, alpha, beta)
+            # pruning
             if value < beta:
                 beta = value
             if alpha >= beta:
@@ -75,5 +83,9 @@ if __name__ == "__main__":
     maxDepth = 4*N
 
     res = minimax(r1, c1, r2, c2)
+    rounds = len(moves)
 
-    print(res)
+    if res < 0:
+        print('BLACK', res+100-1)
+    elif res > 0:
+        print('WHITE', 100-res-1)
